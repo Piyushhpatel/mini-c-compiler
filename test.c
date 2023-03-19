9bec8e32e4bfb33fcd4ef21a2ba46319793182e8
Piyush Patel <piyushpatel1746@gmail.com>
2023-03-20 00:58:02 +0530

    Added test and Result files

---
+++ test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int x=1;
+    int y=2;
+    int z=3;
+    x=3;
+    y=10;
+    // a=5;
+   printf("Hello world");
+   scanf("%d",&z);
+   char w;
+//    w=a;
+
+    return 1;
+}
